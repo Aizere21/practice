3b89d5afa1282b400172891549dcd9e490b3a24d
Aizere <nurmukhamed.aizere21@gmail.com>
2026-09-14 22:50:16 +0500

    Update for loop examples

---
+++ practice 2/loops/for_loop.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+  for x in "banana":
+  print(x)
+
+
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
