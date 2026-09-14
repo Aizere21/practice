dc70af4d014f5109928aa9c1b028b891603630cd
Aizere <nurmukhamed.aizere21@gmail.com>
2026-09-14 23:11:05 +0500

    Add Practice1 Python basics

---
+++ practice 1/python basics/strings.py
@@ -0,0 +1,24 @@
+# 1. 
+name = "Aizere"
+print(name)
+
+# 2. 
+age = "18"
+print(age)
+
+# 3. 
+word = "Python"
+print(len(word))
+
+# 4. 
+text = "hello"
+print(text.upper())
+
+# 5. 
+text = "PYTHON"
+print(text.lower())
+
+# 6. 
+first_name = "Aizere"
+last_name = "Nurukhamed"
+print(first_name + " " + last_name)
